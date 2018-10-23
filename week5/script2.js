2371cd532e7192bf6620f310af3a4bc5644086f6
MaksatMUR1 <44408737+MaksatMUR1@users.noreply.github.com>
2018-10-23 23:05:16 +0600

    Update script2.js

--- week5/script2.js
+++ week5/script2.js
@@ -1,5 +1,16 @@
 <!DOCTYPE html>
 <html>
+var bigIm = document.getElementById("bigImage");
+var carousel = document.getElementById('carousel').getElementsByTagName('img');
+
+for(var i = 0; i<carousel.length; i++){
+    carousel[i].addEventListener('click',changeImage);
+}
+
+function changeImage(){
+    var src = this.getAttribute('src');
+    bigIm.innerHTML = "<img src=" + src + ">";
+}
 
   <head>
     <link rel="stylesheet" href="style3.css">
