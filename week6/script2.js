85b28a0cefb0c20aebfb17c8bf0fabf91f5718fb
MaksatMUR1 <44408737+MaksatMUR1@users.noreply.github.com>
2018-10-23 23:01:59 +0600

    Update and rename week6/script2.js to task6-2.js

--- week6/script2.js
+++
@@ -1,5 +0,0 @@
-let countries = ["Kazakhstan","Russia","England","France"];
-let cities_by_country = {"Kazakhstan":["Almaty","Astana","Karagandy"],"Russia":["Moscow","Saint-Petersburg","Kazan"],"England":["London","Manchester","Liverpool"],"France":["Paris","Lyon","Marseille"]};
-
-/* Write your code here */
-
